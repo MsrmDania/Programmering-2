3209877f354a0dd052539931647cfd5309d6f84c
MsrmDania <maik2274@student.eadania.dk>
2018-05-02 11:59:06 +0200

    seneste version

--- index.php
+++ index.php
@@ -3,39 +3,42 @@
 //Husk at udkommentere dem inden siden sættes i drift!
 //ini_set('error_reporting', E_ALL);
 //ini_set('display_errors', '1')
+?>
+
+<?php
 
  include("opendb.php");
-    $sql = 'SELECT dyrtype, dyrtekst FROM Pets LIMIT 50';
+    $sql = 'SELECT dyrType, dyrAlder, dyrSex, dyrTekst FROM Pets LIMIT 50';
     //echo "sql: " . $sql; //udkommenteres når vi har tjekket at det virker
     $resultat = mysqli_query($conn, $sql); //hent værdier og læg dem i $resultat, så kan vi bruge dem senere
 
 ?>
 
-<!DOCTYPE html>
-<html lang="da">
-<!--Anvendt Bootstrap skabeleon redigeret til eget behov-->
+    <!DOCTYPE html>
+    <html lang="da">
+    <!--Anvendt Bootstrap skabeleon redigeret til eget behov-->
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Programmeringsopgave</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/stylesheet.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/programmering.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Vidaloka" rel="stylesheet">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Programmeringsopgave</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/stylesheet.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/programmering.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Vidaloka" rel="stylesheet">
+    </head>
 
-<body>
-    <header>
-        <!--Navigationsbar-->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <!--Burgermenu-->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-controls="navbar">
+    <body>
+        <header>
+            <!--Navigationsbar-->
+            <nav class="navbar navbar-default navbar-static-top" role="navigation">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <!--Burgermenu-->
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-controls="navbar">
                         <span class="icon-bar">
                             </span>
                         <span class="icon-bar">
@@ -43,28 +46,29 @@
                         <span class="icon-bar">
                             </span>
                         </button>
-                    <!--Burgermenu Slut-->
-                    <a class="navbar-brand" href="index.html"><span class="glyphicon glyphicon-home"> </span></a>
+                        <!--Burgermenu Slut-->
+                        <a class="navbar-brand" href="index.html"><span class="glyphicon glyphicon-home"> </span></a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Beregner <span class="glyphicon glyphicon-pencil"></span></a>
+                                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Behind the Scenes <span class="glyphicon glyphicon-tasks"></span></a>
+                                </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Beregner <span class="glyphicon glyphicon-pencil"></span></a>
-                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Behind the Scenes <span class="glyphicon glyphicon-tasks"></span></a>
-                            </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <div id="pagewrap">
-        <header>
-            <h1>Kæledyr</h1>
+            </nav>
         </header>
-        <form name="dyr_form" method="post" action="behandl_dyr.php">
-            <fieldset>
-                <legend>Mine kæledyr</legend>
-                <label>Typer af kæledyr:</label><br>
-                <select id="dyrType" name="dyrType">
+        <div id="pagewrap">
+            <header>
+                <h1>Kæledyr</h1>
+            </header>
+            <form name="dyr_form" method="post" action="behandl_dyr.php">
+                <fieldset>
+                    <legend>Mine kæledyr</legend>
+                    <label>Typer af kæledyr:</label><br>
+                    <select id="dyrType" name="dyrType">
+                        <option value="0">Vælg kæledyr</option>
                         <option value="Kat">Kat</option>
                         <option value="Hund">Hund</option>
                         <option value="Fugl">Fugl</option>
@@ -79,8 +83,9 @@
                         <option value="Hest">Hest</option>
                         <option value="Andet">Andet</option>
                     </select><br>
-                <label>Kæledyrs alder:</label><br>
-                <select id="dyrAlder" name="dyrAlder">
+                    <label>Kæledyrs alder:</label><br>
+                    <select id="dyrAlder" name="dyrAlder">
+                        <option value="0">Vælg alder</option>
                         <option value="0-2">0 - 2 År</option>
                         <option value="2-4">2 - 4 År</option>
                         <option value="4-6">4 - 6 År</option>
@@ -93,29 +98,30 @@
                         <option value="26-30">26 - 30 År</option>
                         <option value="Over30">Over 30 År</option>
                     </select><br>
-                <label>Køn:</label><br>
-                <select id="dyrSex" name="dyrSex">
+                    <label>Køn:</label><br>
+                    <select id="dyrSex" name="dyrSex">
+                        <option value="0">Vælg køn</option>
                         <option value="Han">Han</option>
                         <option value="Hun">Hun</option>
                         <option value="VedIkke">Ved Ikke</option>
                     </select><br>
-                <label>Navn på kæledyr:</label><br>
-                <textarea id="dyrTekst" name="dyrTekst"></textarea><br>
-                <br>
-                <button type="submit" value="submit">Indsend</button>
-            </fieldset>
-        </form>
-        <section id="pets_form">
-            <?php while ($r = mysqli_fetch_array($resultat)) { //En while-løkke der lægger hver række i $r
-                            echo '<section class=""><h3>' . $r['dyrtype'] . '</h3><p>' . $r['dyrtekst'] . '</p></section>';
-                        } 
+                    <label>Navn på kæledyr:</label><br>
+                    <textarea id="dyrTekst" name="dyrTekst"></textarea><br>
+                    <br>
+                    <button type="submit" value="submit">Indsend</button>
+                </fieldset>
+            </form>
+            <section id="pets_form">
+                <?php while ($r = mysqli_fetch_array($resultat)) { //En while-løkke der lægger hver række i $r
+                            echo '<section class=""><h3>' . $r['dyrType'] . ", " . $r['dyrAlder'] . ", " . $r['dyrSex'] . '</h3><p>' . $r['dyrTekst'] . '</p></section>';
+                        }
                  ?>
-        </section>
+            </section>
+        </div>
+        <!-- pagewrap -->
         <footer>
             <p>&copy; Copyright Maiken Mikkelsen, Anders Vinther, Gurli Tonsberg</p>
         </footer>
-    </div>
-    <!-- pagewrap -->
-</body>
+    </body>
 
-</html>
+    </html>
