7da55abe88872ca66475cf60ee922bf487d25d39
MsrmDania <maik2274@student.eadania.dk>
2018-05-07 14:06:47 +0200

    Sidste rettelser
    Tilføjer video og dokumentation til behind.php og video breakpoints.

--- behind.php
+++ behind.php
@@ -50,20 +50,29 @@
         </nav>
     </header>
     <div id="pagewrap">
+        <div id="video_behind">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/IZ3hgmqUfew?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div id="behind_tekst">
+            <p>Vi har valgt at lave en side, hvor man kan registrere kæledyr. Siden optager cookies for den individuelle<br>bruger og brugeren alarmeres igennem en cookiemeddelelse første gang de besøger hjemmesiden.</p>
+            <p>Sidens formular består af 8 selectors og 3 indtastningsfelter, som alle har validering for at sikre at alle<br>felterne er udfyldt, før de bliver sendt til databasen.</p>
+            <p>Formularen kan først indsendes til databasen når alle felter er udfyldt, dette vises brugeren ved at indsend<br>knappen først er tilgængelig, når brugeren har udfyldt alle felter. Derefter bliver en sætning genereret ud<br>fra brugerens svar, som dukker op nederst på siden som en registrering.</p>
+            <p>Sidens validering er bygget sådan, at hvis et svar ikke er valid bliver det relevante felt oplyst i rødt.<br>Brugerens aktive felt er gult og et valid svar ville blive oplyst i grønt.</p>
+            <p>Dette er til for at kommunikere til brugeren om deres fremskridt i formularen, og for at give dem besked,<br>hvis de mangler at udfylde et felt eller hvis noget der at valgt tæller som et invalid svar. </p>
+            <p>Vi har valgt at kun nogen dataene bliver vist på siden efter at formularen er blevet udfyldt. Bl.a. har vi valgt<br>udlade information såsom om dyret er vaccineret.</p>
+            <p>Formularen står i single column for at gøre den letlæselig og for at hjælpe på tastehastighed for brugeren.<br> Label står over indtastningsfelterne og selectors af samme grund. Vi har valgt at designet skal være enkelt<br>og med bløde, lyse farver.</p>
+            <p>Forbedringer som kunne laves på siden, kunne bestå af muligheden for flere selectors og indtastningsfelter<br>for at få mere konkret data, og at sørge for at inputfelterne er anderledes fra dyr til dyr, så alle<br>valgmuligheder giver mening til dyret.</p>
+            <p>Her kunne der bl.a. være anskaffelses år, født år, døds år, stamtavlenummer og mulighed for at uploade dokumenter mm.</p>
+            <p>Funktionelle forbedringen af siden, kunne bestå et login system, så at brugeren har muligheden for at<br>håndtere deres kæledyr, slette og rette i værdierne.</p>
+        </div>
+        <button onclick="topFunction()" id="myBtn" title="Tilbage til toppen">Top</button>
     </div>
     <footer>
         <p>&copy; Copyright Maiken Mikkelsen, Anders Vinther, Gurli Tonsberg</p>
-        <script type="text/javascript" id="cookieinfo"
-	src="//cookieinfoscript.com/js/cookieinfo.min.js"
-	data-bg="#272627"
-	data-fg="#FFFFFF"
-	data-link="#c1c1c1"
-    data-linkmsg="Mere info"
-    data-message="På vores website bruges cookies til at huske dine indstillinger. Ved fortsat brug af websiden godkender du cookiepolitikken."
-	data-cookie="CookieInfoScript"
-	data-text-align="left"
-    data-close-text="OK!">
-</script>
+        <script type="text/javascript" id="cookieinfo" src="//cookieinfoscript.com/js/cookieinfo.min.js" data-bg="#272627" data-fg="#FFFFFF" data-link="#c1c1c1" data-linkmsg="Mere info" data-message="På vores website bruges cookies til at huske dine indstillinger. Ved fortsat brug af websiden godkender du cookiepolitikken." data-cookie="CookieInfoScript" data-text-align="left" data-close-text="OK!">
+
+
+        </script>
 
     </footer>
 
